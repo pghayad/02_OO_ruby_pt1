8191e649b9eac8289fe9691d32451a769243db3b
Aysan Isayo <aisayo@icloud.com>
2021-09-20 20:33:03 -0700

    starter code

--- starships/README.md
+++ starships/README.md
@@ -1,31 +1,19 @@
-### Phase 3
+### Ruby Object Orientation
 
-#### What are we learning? 
-- Ruby! 
-
-#### Phase Objectives:
-- Understand the principles of Ruby as a language including principles of object oriented programming
-- Understand the characteristics of a relational database
-- Perform CRUD actions with a database using Active Record
-- Design an API to handle CRUD actions
-- Communicate with an API using different HTTP verbs
-- Create and present a project with a React frontend and a database-backed API backend
-
-# Ruby Fundamentals
+#### Lecture Objectives:
+- Demonstrate the power of OO in Ruby
+- Demonstrate the essential steps to implementing OO in Ruby
 
 ### Lecture Deliverables
-- Create a CLI(command line interface) that:
-    - Welcomes and prompts user to input name.
-    - Prints a menu of options: Create a new starship, see a list of starships or exit.
-    - Handles users selection and allows user to continue using app, looping through the program until exit is selected.
+- Create and encapsulate logic for a starship class/instance
 
 ### Lecture Take aways 
-- Variables
-- Methods
-- Conditionals
-- Arrays 
-- Hashes
-- Loops
-- Iteration
-- puts
-- binding.pry
+- Classes
+- Instances
+- Initialize
+- Instance Variables
+- Instance Methods
+- Setter & Getters
+- Attribute Accessors
+- Self
+
--- starships/lib/cli.rb
+++ starships/lib/cli.rb
@@ -1,25 +1,16 @@
-# global variable that stores all the starships as they are created 
-STARSHIPS_ARR = [] # constant variable
-# method 
+STARSHIPS = []
 def init
-  puts "Welcome to the Starships App" # prints text with a line break
+  puts "Welcome to the Starships App"
   puts "Tell us your name: "
-  # print "init" # prints text with no line break
-  # print "second line"
-
-  # need to grab the users input and save it 
-  name = gets.strip # variables 
-  # binding.pry # debugging tool
+  name = gets.strip
   puts "Awesome, #{name.capitalize}, What would you like to do?"
-  # provide a menu 
   menu
-  # handle our users menu selection
+  menu_option
+  goodbye
 end 
 
 def menu
   puts "Enter the number of your selection, or 'exit' to leave the app"
-  # provide the menu options 
-  menu_option
 end 
 
 def menu_option 
@@ -31,22 +22,19 @@ end
 
 def menu_selection
   selection = gets.strip
-  # add control flow to an application 
-while selection != "exit"
-  if selection == "1"
-    # have a method that lets our user create a new starship
-    starship = create_starship
-    STARSHIPS << starship # adding new starship the array using the shovel method
-    # print details of new starship details
-    starship_details(starship)
-  elsif selection == "2"
-    starship_list
-  else 
-    puts "I'm not sure what you mean. Try again:"
+  while selection != "exit"
+    if selection == "1"
+      starship = create_starship
+      STARSHIPS << starship 
+      starship_details(starship)
+    elsif selection == "2"
+      starship_list
+    else 
+      puts "I'm not sure what you mean. Try again:"
+    end 
+    menu_option
+    selection = gets.strip
   end 
-  menu_option
-  selection = gets.strip
-end 
 end 
 
 def create_starship
@@ -55,7 +43,6 @@ def create_starship
   puts "Whats the model?"
   model = gets.strip 
 
-  # hash equivalent to a JS object
   starship = {
     name: name,
     model: model
@@ -71,9 +58,12 @@ def starship_details(starship)
 end 
 
 def starship_list 
-  # iterating over STARSHIPS array
-  # starship is a block variable that represents each element in the array
   STARSHIPS.each do |starship|
     starship_details(starship)
-  end 
+end 
+
+def goodbye
+  puts "Come back again!"
+end 
+
 end 
